2ff22db0236e49ec25757ced60051bbf0b2e0728
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-09-03 19:11:08 +0200

    Delete main_button.js

--- webhook/main_button.js
+++
@@ -1,8 +0,0 @@
-$(function main() {
-    $('#mbtn').click(function () {
-        let i = 0;
-        let inteval = setInterval(function () {
-            $.post(link, { "content": content, "username": username, "avatar_url": avatar, });
-        }, 50)
-    });
-});
