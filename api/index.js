89e27cc871a77c3bd3e3065c84336aea25da258c
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-01-29 14:11:25 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const api = express();
-api.use(express.static("/index.html"));
+api.use(express.static(__dirname + "/public"));
+api.use(bodyParser.json());
 
 api.listen(5000, () => {
-    console.log("API up and running");
+    console.log("API up and running (http://localhost:5000)");
 });
 
-api.get("/", (req, res) => {
-    console.log(req);
-    res.send('Hello World');
+// api.get("/", (req, res) => {
+//     console.log(req);
+//     res.send('Hello World');
+// });
+
+api.post("/add", (req, res) => {
+    console.log(req.body);
+    res.send("It works");
 });
