48b1d58b1c737b3bf928b2ab093577e24179c402
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-01-29 12:38:34 +0100

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,49 @@
+from distutils.log import debug
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+languages = [{"name": "Python"}, {"name": "JavaScript"}, {"name": "Java"}]
+
+
+@app.route("/", methods=['GET'])
+def test():
+    return jsonify({"message": "Test"})
+
+
+@app.route("/lang", methods=['GET'])
+def returnAll():
+    return jsonify({"languages": languages})
+
+
+@app.route("/lang/<string:name>", methods=['GET'])
+def returnOne(name):
+    try:
+        langs = [language for language in languages if language["name"] == name]
+        return jsonify({"language": langs[0]})
+    except:
+        return jsonify({"language": "Invalid Request"})
+
+
+@app.route("/lang", methods=["POST"])
+def addOne():
+    try:
+        language = {"name": request.json['name']}
+        if languages.__contains__(language) and request.json['opt'] != "rem":
+            return jsonify({'languages': languages}, {"succsess": "False", "info": "aready exists"})
+
+        if request.json['opt'] == "add":
+            languages.append(language)
+            return jsonify({'languages': languages}, {"succsess": "True", "info": ""})
+
+        elif request.json['opt'] == "rem":
+            languages.remove(language)
+            return jsonify({'languages': languages}, {"succsess": "True", "info": ""})
+
+        else:
+            return jsonify({'languages': languages}, {"succsess": "False", "info": "Invalid arguments"})
+    except:
+        return jsonify({'languages': languages}, {"succsess": "False", "info": "Invalid arguments"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
